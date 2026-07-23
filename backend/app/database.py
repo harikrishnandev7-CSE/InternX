ec358a3652200636012d1b0bee27c9de15b27bf9
hari-007-krishnan <hari.krishnan.dev7@gmail.com>
2026-07-23 23:27:30 +0530

    engine modified

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 import app.config as config
 from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy.pool import QueuePool
+
 
 def get_db():
     db = SessionLocal()
@@ -9,7 +11,12 @@ def get_db():
     finally:
         db.close()
         
-engine = create_engine(config.DATABASE_URL)
+engine = create_engine(
+    config.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    poolclass=QueuePool,
+)
 
 SessionLocal = sessionmaker(bind=engine)
 
